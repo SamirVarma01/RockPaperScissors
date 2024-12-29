aa8f99941046c8c8b312a60c214aeddfcebbb02e
SamirVarma01 <varma.samir.2005@gmail.com>
2024-12-28 20:27:38 -0500

    Added test scripts

--- scripts/train.py
+++ scripts/train.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 from model import RockPaperScissorsCNN
 from data_prep import train_data, val_data, test_data
 from torch.optim.lr_scheduler import StepLR
+import matplotlib.pyplot as plt
 
 if torch.backends.mps.is_available():
     device = torch.device("mps")  
@@ -19,11 +20,11 @@ val_loader = DataLoader(val_data, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
 scheduler = StepLR(optimizer, step_size=20, gamma=0.1)
 
-num_epochs = 10
-train_losses = []
-val_losses = []
+num_epochs = 100
 train_accuracies = []
 val_accuracies = []
+train_losses = []
+val_losses = []
 
 for epoch in range(num_epochs):
     model.train()
@@ -46,8 +47,10 @@ for epoch in range(num_epochs):
 
     epoch_loss = running_loss / len(train_loader)
     epoch_accuracy = 100 * correct / total
+    train_losses.append(epoch_loss)
+    train_accuracies.append(epoch_accuracy)
 
-    if (epoch + 1) % 1 == 0:
+    if (epoch + 1) % 10 == 0:
         print(f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.2f}%")
     scheduler.step()
 
@@ -70,8 +73,10 @@ for epoch in range(num_epochs):
 
     val_loss /= len(val_loader)
     val_accuracy = 100 * val_correct / val_total
+    val_losses.append(val_loss)
+    val_accuracies.append(val_accuracy)
 
-    if (epoch + 1) % 1 == 0:
+    if (epoch + 1) % 10 == 0:
         print(f"Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.2f}%")
 
 
@@ -92,6 +97,26 @@ with torch.no_grad():
 
 test_loss /= len(test_loader)
 test_accuracy = 100 * test_correct / test_total
+
+plt.figure(figsize=(10, 5))
+plt.plot(range(1, num_epochs + 1), train_accuracies, label="Training Accuracy")
+plt.plot(range(1, num_epochs + 1), val_accuracies, label="Validation Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy (%)")
+plt.title("Training and Validation Accuracy")
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.figure(figsize=(10, 5))
+plt.plot(range(1, num_epochs + 1), train_losses, label="Training Loss", color="green")
+plt.plot(range(1, num_epochs + 1), val_losses, label="Validation Loss", color="red")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.title("Training and Validation Loss")
+plt.legend()
+plt.grid()
+plt.show()
 
 print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%")
 torch.save(model.state_dict(), 'rock_paper_scissors_model.pth')
